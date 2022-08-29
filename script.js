dd249e427db4a1a1fbdd33fde65b314a3cd93dd6
rid6353 <naboleridwane001@gmail.com>
2022-08-29 19:33:37 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const openNav = document.querySelector(".icon1")
+const fermerNav = document.querySelector(".fermer")
+const menu = document.querySelector(".menu")
+
+const positionMenu = menu.getBoundingClientRect().left;
+
+openNav.addEventListener("click", () =>
+{
+    if(positionMenu < 0)
+    {
+        menu.classList.add("menuMini")
+    }
+})
+
+fermerNav.addEventListener("click", () =>
+{
+    if (positionMenu < 0) 
+    {
+        menu.classList.remove("menuMini")   
+    }
+})
